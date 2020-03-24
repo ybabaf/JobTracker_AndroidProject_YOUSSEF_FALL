a10785560f4343097cee397dfb6899dea420d734
aliYous <aliyoussef34@hotmail.fr>
2020-03-25 00:27:57 +0100

    See Job Offer page functionnal, adding link to the offer online in webview

--- app/src/main/java/fr/esilv/a4/ibo6/yousseffall/jobtracker/activity/SeeOfferActivity.kt
+++ app/src/main/java/fr/esilv/a4/ibo6/yousseffall/jobtracker/activity/SeeOfferActivity.kt
@@ -2,14 +2,42 @@ package fr.esilv.a4.ibo6.yousseffall.jobtracker.activity
 
 import android.nfc.Tag
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
+import com.squareup.picasso.Picasso
 import fr.esilv.a4.ibo6.yousseffall.jobtracker.R
 
 class SeeOfferActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_offer)
+
+        /*
+        Getting extra values from intent
+         */
+        val intent = intent
+        val title = intent.getStringExtra("JOB_OFFER_TITLE")
+        val company = intent.getStringExtra("JOB_OFFER_COMP")
+        val image = intent.getStringExtra("JOB_OFFER_IMAGE")
+        val location = intent.getStringExtra("JOB_OFFER_LOC")
+        val type = intent.getStringExtra("JOB_OFFER_TYPE")
+        val description = intent.getStringExtra("JOB_OFFER_DESC")
+
+        findViewById<TextView>(R.id.textViewTitle).text = title
+        findViewById<TextView>(R.id.textViewCompany).text = company
+        findViewById<TextView>(R.id.textViewLocation).text = location
+        findViewById<TextView>(R.id.textViewType).text = type
+        val txtViewDesc = findViewById<TextView>(R.id.textViewDescription)
+        txtViewDesc.text = HtmlCompat.fromHtml(description, 0)
+        txtViewDesc.movementMethod = ScrollingMovementMethod()
+        val imageView = findViewById<ImageView>(R.id.imageLogoEnt)
+        Picasso.get().load(image).into(imageView);
+
+
     }
 
 }
--- app/src/main/java/fr/esilv/a4/ibo6/yousseffall/jobtracker/adapter/JobOfferAdapter.kt
+++ app/src/main/java/fr/esilv/a4/ibo6/yousseffall/jobtracker/adapter/JobOfferAdapter.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import fr.esilv.a4.ibo6.yousseffall.jobtracker.R
+import fr.esilv.a4.ibo6.yousseffall.jobtracker.activity.SeeOfferActivity
 import fr.esilv.a4.ibo6.yousseffall.jobtracker.model.JobOffer
 import kotlinx.android.synthetic.main.layout_offer.view.*
+
 
 class JobOffersAdapter(private val jobOffers : List<JobOffer>) : RecyclerView.Adapter<JobOffersAdapter.JobOfferViewHolder>() {
 
@@ -28,6 +30,17 @@ class JobOffersAdapter(private val jobOffers : List<JobOffer>) : RecyclerView.Ad
         holder.view.textViewTitle.text = jobOffer.title
         holder.view.textViewCompany.text = jobOffer.company
         holder.view.textViewLocation.text = jobOffer.location //HtmlCompat.fromHtml(jobOffer.description, 0)
+
+        holder.view.cardViewOffer.setOnClickListener(({
+            val intent = Intent(holder.view.context, SeeOfferActivity::class.java)
+            intent.putExtra("JOB_OFFER_TITLE", jobOffer.title)
+            intent.putExtra("JOB_OFFER_COMP", jobOffer.company)
+            intent.putExtra("JOB_OFFER_IMAGE", jobOffer.company_logo)
+            intent.putExtra("JOB_OFFER_LOC", jobOffer.location)
+            intent.putExtra("JOB_OFFER_TYPE", jobOffer.type)
+            intent.putExtra("JOB_OFFER_DESC", jobOffer.description)
+            holder.view.context.startActivity(intent)
+        }))
 
         /*holder.view.buttonSeeOffer.setOnClickListener(({
             val intent = Intent(Context context, SeeOfferActivity::class.java)
